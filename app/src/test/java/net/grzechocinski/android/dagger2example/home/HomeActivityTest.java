2574587c6660086b88402bbd41cd0234139bce00
MasonLiu <masonliuchn@gmail.com>
2015-06-19 15:55:34 +0800

    命令行没有问题

--- app/src/test/java/net/grzechocinski/android/dagger2example/home/HomeActivityTest.java
+++ app/src/test/java/net/grzechocinski/android/dagger2example/home/HomeActivityTest.java
@@ -1,7 +1,10 @@
 package net.grzechocinski.android.dagger2example.home;
 
+import android.content.Context;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
